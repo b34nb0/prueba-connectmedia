583d27522216d7f18e96e8ce5befc7b6b2827653
b34nb0 <fbhirakata@gmail.com>
2020-10-11 18:14:26 -0500

    Prepare files for deployment in heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -22,8 +23,17 @@ connection.once('open', () => {
 
 // Routes
 const productsRouter = require('./routes/products');
-
 app.use('/products',productsRouter);
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 // Start up server
